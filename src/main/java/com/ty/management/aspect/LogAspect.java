1f9b55ab54214f16e90703be2daebc21aaaf6ac2
tianyuan <tiany1992@sina.com>
2017-12-20 23:40:16 +0800

    完善日志打印，增加around功能

--- src/main/java/com/ty/management/aspect/LogAspect.java
+++ src/main/java/com/ty/management/aspect/LogAspect.java
@@ -1,32 +1,61 @@
 package com.ty.management.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Aspect
 @Component
 public class LogAspect {
     private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
-    @Before("execution(* com.ty.management.controller.*Controller.*(..))")
-    public void beforeMethod(JoinPoint joinPoint){
-        StringBuilder sb = new StringBuilder();
-        for(Object arg : joinPoint.getArgs()){
-            sb.append("arg:" + arg.toString() + "|");
+    @Pointcut("execution(* com.ty.management.controller.*Controller.*(..))")
+    public void controllerAspect(){
+
+    }
+
+    @Before("controllerAspect()")
+    public void doBefore(JoinPoint  joinPoint){
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    @AfterReturning(returning="obj", pointcut="controllerAspect()")
+    public void after(Object obj) throws Throwable{
+        logger.info("RESPONSE:" + obj.toString());
+    }
+
+    @Around("controllerAspect()")
+    public Object around(ProceedingJoinPoint joinPoint)throws Throwable{
+        Object returnVal = joinPoint.proceed();
+        long start = System.currentTimeMillis();
+        try{
+            joinPoint.proceed();
+            long end = System.currentTimeMillis();
+            logger.info("around" + joinPoint + "user time :" + (end-start) + "ms!");
+        }catch (Throwable e){
+            long end = System.currentTimeMillis();
+            logger.info("around" + joinPoint + "user time with exception:" + (end-start) + "ms!");
         }
-        sb.append(joinPoint.toString());
-
-        logger.info("before method: " + sb);
-
+        //一定要有这步，否则无法正常运行
+        //返回对象
+        return returnVal;
     }
 
-    @After("execution(* com.ty.management.controller.*Controller.*(..))")
-    public void afterMethod(){
-        logger.info("after method: ");
-    }
 }
